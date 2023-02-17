bff90acc6e961eda39d40194e45be613183b0283
unknown <kiseleva2301@yandex.ru>
2023-02-17 17:11:07 +0300

    feat: change rates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+
 import './App.css';
 import Rate from './Rate/Rate';
+
 
 const allrates = [
   { price: 300, speed: 10, color: 'blue' },
   { price: 450, speed: 50, color: 'green' },
-  { price: 550, speed: 100, color: 'red', isSelect: true },
+  { price: 550, speed: 100, color: 'red' },
   { price: 1000, speed: 200, color: 'black' }
 ]
 
 
 function App() {
+
   return (
     <div className="App">
       {allrates.map((allrate, index) =>
--- src/Rate/Rate.jsx
+++ src/Rate/Rate.jsx
@@ -1,19 +1,30 @@
+import { useState } from 'react';
 import CSSModules from 'react-css-modules';
 import style from './style.module.scss';
 
-function Rate({ price, speed, color, isSelect }) {
+
+
+
+function Rate({ price, speed, color, isSelect, onSetSelect }) {
 
     let styleCard = color;
-
 
     if (isSelect) {
         styleCard += 'cardSelected';
     }
 
+
+    function onClick() {
+        onSetSelect(styleCard += 'cardSelected');
+    }
+    const stateArr = useState(styleCard);
+    styleCard = stateArr[0];
+    onSetSelect = stateArr[1];
+
+
+
     return (
-
-
-        <div styleName={styleCard}>
+        <div styleName={styleCard} onClick={onClick}>
             <div styleName="cardName">Безлимитный {price}</div>
             <div styleName="cardPrice">{price} руб/мес</div>
             <div styleName="cardSpeed">до {speed} Мбит/сек</div>
